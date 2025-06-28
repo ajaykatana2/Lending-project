eaf5cfcdcf4babc23dc8652f774ba6919beba3cc
Ajay Katana <150595427+ajaykatana2@users.noreply.github.com>
2025-06-28 17:21:12 +0530

    Update lending-project.sol

--- contract/lending-project.sol
+++ contract/lending-project.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/math/Math.sol";  
 
-
+    
 
 /** 
  * @title LendingProtocol
